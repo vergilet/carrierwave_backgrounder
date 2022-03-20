b954364a6d32c097bf7e4ed7f1fc66fa80d495c7
Yaro <vergilet@users.noreply.github.com>
2022-03-20 09:27:54 +0200

    Update process_asset_mixin.rb

--- lib/backgrounder/workers/process_asset_mixin.rb
+++ lib/backgrounder/workers/process_asset_mixin.rb
@@ -22,7 +22,7 @@ module CarrierWave
           pp 'in'
           record.send(:"process_#{column}_upload=", true)
           pp 'zzz'
-          x = record.send(:"#{column}").recreate_versions!(record.send(:"#{column}").keys)
+          x = record.send(:"#{column}").recreate_versions!(record.send(:"#{column}").versions.keys)
           y = record.respond_to?(:"#{column}_processing")
           pp x
           pp y
