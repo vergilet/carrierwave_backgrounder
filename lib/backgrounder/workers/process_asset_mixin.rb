888993b13132f6059a871d7b6091bb88ae142f1a
Yaro <vergilet@users.noreply.github.com>
2022-03-21 09:43:12 +0200

    Update process_asset_mixin.rb

--- lib/backgrounder/workers/process_asset_mixin.rb
+++ lib/backgrounder/workers/process_asset_mixin.rb
@@ -11,31 +11,24 @@ module CarrierWave
 
       def perform(*args)
         record = super(*args)
-
-        x = record && record && record.send(:"#{column}").present?
-        pp 'before'
-        pp record.inspect
-        pp record.send(:"#{column}").present?
         
-        
-        if x
-          pp 'in'
+        if record && record.send(:"#{column}").present?
           record.send(:"process_#{column}_upload=", true)
-          pp 'zzz'
-          x = record.send(:"#{column}").recreate_versions!(record.send(:"#{column}").versions.keys)
-          y = record.respond_to?(:"#{column}_processing")
-          pp x
-          pp y
-          pp 'V'
-          if x && y
-            pp "good"
-            pp "My args: #{args.inspect}"
+          if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
             record.update_attribute :"#{column}_processing", false
           end
         else
-          pp "not ready"
-          pp "My args: #{args.inspect}"
           when_not_ready
+        end
+
+
+        if args[:versions_process] && args[:remote_image_url]
+          record.send(:"process_#{column}_upload=", true)
+          record.send(:"remote_#{column}_url=", args[:remote_image_url])
+          record.send(:"#{column}").recreate_versions!
+        else
+          remote_image_url = record.reload.send(:"#{column}").url
+          self.class.perform_async(*args.merge(versions_process: true, remote_image_url: remote_image_url))
         end
       end
 
