fab5cd17664fb1a55625c21b4afc26a1067072cf
Yaro <vergilet@users.noreply.github.com>
2022-03-21 13:57:15 +0200

    Update process_asset_mixin.rb

--- lib/backgrounder/workers/process_asset_mixin.rb
+++ lib/backgrounder/workers/process_asset_mixin.rb
@@ -20,25 +20,6 @@ module CarrierWave
         else
           when_not_ready
         end
-
-        key = record.send(:"#{column}").versions.keys.first
-        version_file = record.send(:"#{column}").send(key).file
-        pp 'version file'
-        pp version_file.inspect
-
-        if version_file.blank?
-          pp 'no version'
-          pp 'try get original url'
-          pp record.reload.send(:"#{column}").url
-          record.send(:"process_#{column}_upload=", true)
-          record.send(:"remote_#{column}_url=", record.reload.send(:"#{column}").url)
-          record.send(:"#{column}").recreate_versions!
-          pp 'maybe need save?'
-        else
-          pp '2nd start of worker'
-          pp args
-          self.class.perform_async(*args)
-        end
       end
 
     end # ProcessAssetMixin
